e6b315a038e089c889803ffe5e779a1fa6b6b098
Bharat Singh Rajput <34980375+iambharatsingh@users.noreply.github.com>
2018-05-17 23:05:02 +0530

    Create Arithmetic Operators.py

---
+++ Arithmetic Operators.py
@@ -0,0 +1,29 @@
+from __future__ import print_function
+#ARITHMETIC OPERATORS
+
+'''
+Beside +,-,/,*,% We Have -
+
+** - Exponential Calculations
+// - Floor Division (Discards Decimal)
+
+'''
+
+#EXAMPLES
+
+print ("5 + 2 = ", 5 + 2)
+
+print ("5 - 2 = ", 5 - 2)
+
+print ("5 / 2  = ", 5.0 / 2)
+
+print ("5 * 2 = ", 5 * 2)
+
+print ("5 % 2 = ", 5 % 2)
+
+print ("5 ** 2 = ", 5 ** 2)
+
+print ("5 // 2 = ", 5.0 // 2)
+
+
+
