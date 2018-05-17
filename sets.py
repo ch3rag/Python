ab26b3ff8e57d3f3d31b5309a64d09c97305db25
Bharat Singh Rajput <34980375+iambharatsingh@users.noreply.github.com>
2018-05-18 03:48:41 +0530

    Create sets.py

---
+++ sets.py
@@ -0,0 +1,23 @@
+from __future__ import print_function
+
+vowels = {"a" , "e", "i", "o", "u", "a"}
+
+print(vowels)
+
+# DUPLICATES ARE REMOVED AUTOMATICALLY
+
+a = {1, 5, 8, 9, 17, 15, 21}
+
+b = {5, 34, 17, 18, 19, 21, 24, 36}
+
+print(a - b)
+
+print(a & b)
+
+print(a | b)
+
+print(a ^ b)
+
+multiples = {x * 2 for x in range(1,10)}
+
+print(multiples)
